8338cec96d3c4ddccf5a9e377ba713f855a7d13c
alesiahrechka <alesiahrechka@gmail.com>
2014-10-21 14:22:31 +0300

    Change files: pom.xml

---
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -0,0 +1,25 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.domain.User;
+
+import java.util.List;
+/**
+ * Created by alesya on 21.10.14.
+ */
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public void removeUser(Long userId) {
+
+    }
+}
