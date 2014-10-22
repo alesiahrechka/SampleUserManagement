462e2f97b5e29ccaec65bc1012e8cb6e3c562001
alesiahrechka <alesiahrechka@gmail.com>
2014-10-22 20:40:58 +0300

    good test

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -2,18 +2,22 @@ package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 /**
  * Created by alesya on 21.10.14.
  */
 public class UserDaoImpl implements UserDao {
 
-    private JdbcTemplate jdbctemplate;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        jdbctemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
@@ -23,11 +27,23 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return jdbcTemplate.query("select userid, login, name from USER", new UserMapper());
     }
 
     @Override
     public void removeUser(Long userId) {
 
+    }
+
+    public class UserMapper implements RowMapper<User> {
+
+
+        public User mapRow(ResultSet rs, int i) throws SQLException {
+            User user = new User();
+            user.setUserId(rs.getLong("userid"));
+            user.setLogin(rs.getString("login"));
+            user.setUserName(rs.getString("name"));
+            return user;
+        }
     }
 }
