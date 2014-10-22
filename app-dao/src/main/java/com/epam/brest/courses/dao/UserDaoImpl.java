b3bb2541a26ad8d2f67e8a5d80f70159c64b3573
alesiahrechka <alesiahrechka@gmail.com>
2014-10-22 20:22:54 +0300

    test

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -1,12 +1,21 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 /**
  * Created by alesya on 21.10.14.
  */
 public class UserDaoImpl implements UserDao {
+
+    private JdbcTemplate jdbctemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        jdbctemplate = new JdbcTemplate(dataSource);
+    }
+
 
     @Override
     public void addUser(User user) {
--- app-dao/src/test/java/com.epam.brest.courses.dao/UserDaoImplTest.java
+++ app-dao/src/test/java/com.epam.brest.courses.dao/UserDaoImplTest.java
@@ -1,12 +1,25 @@
 package com.epam.brest.courses.dao;
-
+import com.epam.brest.courses.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.List;
+import static org.junit.Assert.*;
 
 /**
  * Created by alesya on 22.10.14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/testApplicationContextSpring.xml" })
 public class UserDaoImplTest {
-
     @Autowired
     private UserDao userDao;
+
+    @Test
+    public void getUsers() {
+        List<User> users = userDao.getUsers();
+        assertNotNull(users);
+    }
 }
